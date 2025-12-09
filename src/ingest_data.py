3daee4bd642050c403d272e2982f27ff7dcd813c
adityakanamadis@gamil.com <adityakanamadis@gmail.com>
2025-12-09 23:28:12 +0530

    Data Ingestion

---
+++ src/ingest_data.py
@@ -0,0 +1,62 @@
+import os 
+import zipfile 
+from abc import ABC, abstractmethod 
+import pandas as pd 
+
+
+class DataIngestor(ABC):
+    @abstractmethod 
+    def ingest(self, file_path: str) -> pd.DataFrame:
+        """ Abstract method to ingest data from a file path."""
+        pass 
+
+
+class ZipDataIngestor(DataIngestor):
+    def ingest(self, file_path: str) -> pd.DataFrame:
+        """Extracts a zipfile and returns the content as a pandas DataFrame."""
+        if not file_path.endswith(".zip"):
+            raise ValueError("Provided file is not a zipfile.")
+        
+        with zipfile.ZipFile(file_path, "r") as zip_ref:
+            zip_ref.extractall("extracted_data")
+
+
+        # Find the extracted csv file 
+        extracted_files = os.listdir("extracted_data")
+        csv_files = [f for f in extracted_files if f.endswith(".csv")]
+
+        if len(csv_files) == 0:
+            raise FileNotFoundError(" No CSV file found in the extracted data.")
+        
+        if len(csv_files) > 1:
+            raise ValueError("Multiple CSV files found. Please specify which one to use.")
+        
+        csv_file_path = os.path.join("extracted_data", csv_files[0])
+        df=pd.read_csv(csv_file_path)
+
+        return df 
+    
+
+class DataIngestorFactory:
+    @staticmethod
+    def get_data_ingestor(file_extension: str) -> DataIngestor:
+        """Returns the approprate DataIngestor based on file extension."""
+        if file_extension == ".zip":
+            return ZipDataIngestor()
+        else:
+            raise ValueError(f"No ingestor available for file extension: {file_extension}")
+        
+
+
+if __name__ == "__main__":
+
+    file_path = r"C:\Users\adity\Production_Ready_ML_Solutions\data\archive.zip"
+
+    file_extension = os.path.splitext(file_path)[1]
+
+    data_ingestor = DataIngestorFactory.get_data_ingestor(file_extension)
+
+    df = data_ingestor.ingest(file_path)
+
+    print(df.head())
+    pass 
